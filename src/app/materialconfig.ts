d9c826e923226af4b36b453fbfa221f22dd1f84a
VinceX2 <vince.geek404@gmail.com>
2018-10-26 01:09:24 -0700

    Sidebar update

--- src/app/materialconfig.ts
+++ src/app/materialconfig.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 // Angular Material Components
 import { MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-    imports: [
-        MatSidenavModule,
-        MatToolbarModule
-    ],
-    exports: [
-        MatSidenavModule,
-        MatToolbarModule
-    ],
+  imports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule],
+  exports: [MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule]
 })
-export class MaterialModule { }
+export class MaterialModule {}
