742a6a10f408260f9f9883a83ef3fc6e19c60b91
VinceX2 <vince.geek404@gmail.com>
2018-11-08 23:51:52 -0800

    Modificando el Toolbar

--- src/app/components/toolbar-options/toolbar-options.component.ts
+++ src/app/components/toolbar-options/toolbar-options.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Food {
+  value: string;
+  viewValue: string;
+}
+
+
 @Component({
   selector: 'app-toolbar-options',
   templateUrl: './toolbar-options.component.html',
   styleUrls: ['./toolbar-options.component.css']
 })
 export class ToolbarOptionsComponent implements OnInit {
-
+  foods: Food[] = [
+    { value: 'steak-0', viewValue: 'Steak' },
+    { value: 'pizza-1', viewValue: 'Pizza' },
+    { value: 'tacos-2', viewValue: 'Tacos' }
+  ];
   constructor() { }
 
   ngOnInit() {
--- src/app/materialconfig.ts
+++ src/app/materialconfig.ts
@@ -5,7 +5,7 @@ import { NgModule } from '@angular/core';
 import { MatSidenavModule, MatToolbarModule } from '@angular/material';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import {MatSelectModule} from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
